b672f219de556bab05825588d44d79c5d005625b
suraj <surajgautam56878@gmail.com>
2025-06-30 00:06:17 +0530

    utils- apierror, asyncHandler and ApiResponse

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); // Limit JSON body size to 16kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Limit URL-encoded body size to 16kb
+app.use(express.static("public")); // Serve static files from the "public" directory
+app.use(cookieParser()); // Parse cookies from incoming requests
+export { app };
