8a3479b8aadafb8deeea792d00d8f61a59b7fc9f
suraj <surajgautam56878@gmail.com>
2025-06-30 14:08:43 +0530

    all setup config

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,4 +14,10 @@ app.use(express.json({ limit: "16kb" })); // Limit JSON body size to 16kb
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Limit URL-encoded body size to 16kb
 app.use(express.static("public")); // Serve static files from the "public" directory
 app.use(cookieParser()); // Parse cookies from incoming requests
+
+import userRouter from "./routes/user.routes.js";
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
